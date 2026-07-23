17f9debeda7498c2e5745a9e57a59d824d65cc96
xlebusek-kate <iriskadeniska195@gmail.com>
2026-07-23 10:31:52 +0300

    Введение в базы данных

---
+++ src/test/java/org/skypro/_9999/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro._9999;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
